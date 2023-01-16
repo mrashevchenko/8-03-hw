e0b9afc048009853cf0e1cd4a386f6a052d04c2d
rashevchenkma <mmmrrraaa@yandex.ru>
2023-01-16 17:22:51 +0300

    master, test 1

---
+++ .bash_history
@@ -0,0 +1,8 @@
+sudo adduser mikhailr sido
+sudo adduser mikhailr sudo
+su
+Оценивая решения по Вы вносите вклад в развитие тех поддержки!
+Добрый день!
+МП заменена.
+Спасибо за обращение. 
+su
---
+++ case.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+echo "Which color do you ltke best?" echo
+"1 - Blue" echo
+"2 - Red" echo
+"3 - Yellow" echo
+"4 - Green" echo "5 - Orange" read color:
+case Scolor in
+﻿﻿﻿echo "Blue is a primary color.":;
+﻿﻿﻿echo "Red is a primary color."
+﻿﻿﻿echo "Yellow is a primary color.
+﻿﻿﻿echo "Green is a secondary color.
+﻿﻿﻿echo "Orange is a secondary color.
+*) echo "This color is not available. Please choose a different one.";;
